a54e7a458057d148a1c7fb4183357a6f7930dd73
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-02-06 19:45:19 -0800

    more work on schemas

--- test/component.js
+++ test/component.js
@@ -1,9 +1,45 @@
 'use strict';
 
-describe('component', () => {
-  it('empty', done => {
-    done();
-  });
+const Ajv = require('ajv');
+const ajv = new Ajv();
+const lib = require('../lib/');
+
+describe('any', () => {
+  const validator = ajv.compile(lib.any);
+  [{
+    component: {
+      name: 'foo', library: 'bar', vendor: 'baz', version: '0.1.0',
+      busInterfaces: [],
+      model: {ports: []}
+    }
+  }, {
+    abstractionDefinition: {
+      name: 'foo', library: 'bar', vendor: 'baz', version: '0.1.0',
+      busType: {
+        name: 'foo', library: 'bar', vendor: 'baz', version: '0.1.0'
+      },
+      ports: {
+        CMD: {
+          description: 'command',
+          wire: {
+            onMaster: {direction: 'out'},
+            onSlave: {direction: 'in'}
+          }
+        }
+      }
+    }
+  }]
+    .map((duh, idx) => {
+      it((duh.name || 's' + idx), done => {
+        if (validator(duh)) {
+          done();
+        } else {
+          console.log(validator.errors);
+          throw new Error();
+        }
+      });
+    });
+
 });
 
 /* eslint-env mocha */
