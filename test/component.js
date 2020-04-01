fffe0f4878bc87d6de6804b014eb4329e83930ef
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2020-04-01 15:59:24 -0700

    validate schema with JSON meta schema

--- test/component.js
+++ test/component.js
@@ -1,7 +1,16 @@
 'use strict';
 
 const Ajv = require('ajv');
+const meta = require('ajv/lib/refs/json-schema-draft-06.json');
 const lib = require('../lib/');
+
+describe('meta', () => {
+  it('any', async () => {
+    const ajv = new Ajv({schemaId: 'auto'});
+    const validate = ajv.addMetaSchema(meta).compile({});
+    validate(lib.any);
+  });
+});
 
 describe('any', () => {
   const ajv = new Ajv();
